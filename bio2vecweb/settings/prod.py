0139fcc830262090182b3da37e1e71464fd95b7b
Maxat Kulmanov <coolmaksat@gmail.com>
2019-11-27 14:06:55 +0300

    update prod elasticsearch address

--- bio2vecweb/settings/prod.py
+++ bio2vecweb/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
 
 DEBUG = False
-ELASTIC_INDEX_URL = 'http://10.254.145.46:9200/bio2vec_prod'
+ELASTICSEARCH_URL = 'http://10.127.4.79:9200/'
 BIO2VEC_API_URL = 'http://localhost/api/bio2vec/'
